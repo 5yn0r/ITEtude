9f7c93a69b5a524610d4ad7c09039683d5b2b870
ViMa <vima40422@gmail.com>
2026-01-25 21:22:32 +0000

    Finalisation de la version 2

--- src/app/(app)/ressources/[slug]/page.tsx
+++ src/app/(app)/ressources/[slug]/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { AppHeader } from "@/components/app-header";
@@ -54,12 +55,12 @@ export default function ResourcePage({ params }: { params: { slug: string } }) {
   return (
     <>
       <AppHeader title={category.name} />
-      <main className="flex-1 flex flex-col min-h-0 bg-secondary/50">
+      <main className="flex-1 min-h-0 bg-secondary/50 overflow-y-auto">
         <div className="sticky top-0 z-20 border-b bg-background p-2">
           <FilterControls onFilterChange={setFilters} />
         </div>
         
-        <div className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
+        <div className="p-4 md:p-6 lg:p-8">
           {loading ? (
              <ResourceGridSkeleton />
           ) : (
